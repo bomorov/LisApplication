beba3cea76837655130fde6ca67cd4ffb412798f
bomorov <bomorov1998@gmail.com>
2021-01-04 10:37:23 +0600

    CreatePost Method on Region

---
+++ Application/Models/Result.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Application.Common.Models
+{
+    public class Result
+    {
+        public Result(bool succeeded, string title, string type, IEnumerable<string> messages)
+        {
+            Succeeded = succeeded;
+            AlertTitle = title;
+            AlertType = type;
+            Messages = messages.ToArray();
+        }
+
+        public bool Succeeded { get; set; }
+        public string AlertType { get; set; }
+        public string AlertTitle { get; set; }
+        public string[] Messages { get; set; }
+
+        public static Result Success(List<string> messages = null)
+        {
+            if (messages == null)
+                messages = new List<string>();
+            messages.Add("Успешно: процедура выполнилась");
+            return new Result(true, "Успешно!", "success", messages);
+        }
+
+        public static Result Failure(IEnumerable<string> errors) => new Result(false, "Ошибка", "error", errors);
+
+        public static Result Failure(string error) => new Result(false, "Ошибка", "error", new List<string> { error });
+    }
+}
--- WebUI/Controllers/DistrictController.cs
+++ WebUI/Controllers/DistrictController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.MediatR.Districts.Commands;
 using Application.MediatR.Districts.Queries;
 
 namespace WebUI.Controllers
@@ -13,6 +14,15 @@ namespace WebUI.Controllers
         {
             var districts = await Mediator.Send(query);
             return Ok(districts);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateDistrictCommand command)
+        {
+            var (result, id) = await Mediator.Send(command);
+            if (!result.Succeeded)
+                return BadRequest(result.Messages);
+            return Ok(command);
         }
     }
 }
